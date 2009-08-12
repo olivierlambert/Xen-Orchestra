639b2a3835fc6323ff009738c2752c861fcdbbd2
olivier <olivier@work.(none)>
2009-08-12 20:51:56 +0200

    Clean code

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
 include "inc/header.php";
 include "inc/config.php";
 
-/*
-$user = "toto";
-$pass = "toto";
-$dom01 = "xena1";
-$dom02 = "xenb1";
-$port = "9363";
-$dbname = "database/vm";
-*/
-
 
 for ($i=0;$i<count($domain);$i++)
 {
@@ -20,10 +11,6 @@ for ($i=0;$i<count($domain);$i++)
 	$_SESSION["'.$i.'"] = serialize($dom0);
 }
 
-include "inc/footer.php";
-/*
-$xena1 = new Dom0($dom01,$port,$user,$pass);
 
-$_SESSION['dom0'] = serialize($xena1);
-include "inc/footer.php";*/
+include "inc/footer.php";
 ?>
