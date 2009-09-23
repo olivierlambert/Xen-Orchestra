39a30aef52f3bd65157a94cb9597145937ebe0a6
Olivier Lambert <lambert.olivier@gmail.com>
2009-09-23 12:23:59 +0200

    Change conf file to allow modification without propagating on the repository

--- install.php
+++ install.php
@@ -1,36 +1,24 @@
 <?php
 require_once dirname (__FILE__) . '/includes/prepend.php';
-$cfg = Config::get_instance();
 
+$cfg = Config::get_instance();
 $db = Db::get_instance();
 
 
 
-
-
-echo '<p>Database Creation :</p>';
+echo '<h1>Database Creation :</h1>';
 
 
 try {
-	//$db->query('DROP TABLE dom0');
-	//$db->query('DROP TABLE domU');
-	/*
-	$db->query('CREATE TABLE dom0 (id varchar(128), object text)');
-	$db->query('CREATE TABLE domU (vm_name varchar(128), state varchar(128), domN int)');
-	$db->query('CREATE TABLE network (id int, object text)');
-	$db->query('CREATE TABLE vif (id int, object text)');
-	$db->query('CREATE TABLE vm (id int, object text)');
-	$db->query('CREATE TABLE metrics (id int, object text)');*/
 	$db->query('DROP TABLE dom0');
 	$db->query('DROP TABLE domU');
-	//$db->query('DROP TABLE migrated');
 	$db->query('CREATE TABLE dom0 (id varchar(128), object text)');
 	$db->query('CREATE TABLE domU (vm_name varchar(128), state varchar(128), domN int)');
 }
 catch (Exception $e) {
-	echo "Error : ",$e->getMessage(), "<br/>";
+	echo '<p>Error : ',$e->getMessage().'</p>';
 	die();
 }
 
-echo "Database successfully generated <br/>";
+echo '<p>Database successfully generated.</p>';
 
