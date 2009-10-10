cba6fd7cbe5d8c24a5a65f8cf60d4c9bdf2f7f06
Julien Fontanet <julien.fontanet@isonoe.net>
2009-10-10 22:15:37 +0200

    PHP Classes files moved from libs/classes/ to just libs/.

--- includes/prepend.php
+++ includes/prepend.php
@@ -10,5 +10,5 @@ set_include_path (ROOT_DIR . PATH_SEPARATOR . get_include_path ());
 // autoload classes
 function __autoload($class_name)
 {
-	require_once 'libs/classes/' . $class_name . '.php';
+	require_once 'libs/' . $class_name . '.php';
 }
