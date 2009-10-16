6644bdc90b4ea3e3162fc48e52b9b72e00ccfdc5
Olivier Lambert <lambert.olivier@gmail.com>
2009-10-16 15:29:32 +0200

    Major advance in VM window design. Almost done. Lot's of css change, plus css reorganisation. Moreover, new icons added for actions (play pause stop etc).

--- htdocs/scripts/functions.js
+++ htdocs/scripts/functions.js
@@ -35,9 +35,10 @@ function html_cpu_meters(cpus)
 	return result;
 }
 
-function display_vm(name, id, n)
+function display_vm(name, id, dom0, idwindow)
 {
-
+	// TO DO : prepare refresh if value or action is maded (give id of windows then refresh it)
+	// see idwindow for that
 	var url = 'display_domU.php?name='+name+'&id='+id+'';
 	var req = new Ajax.Request(url,
 	{
@@ -46,46 +47,63 @@ function display_vm(name, id, n)
 		{
 			var result = transport.responseText;
 			var json = result.evalJSON();
-			win = new Window(
+			if (idwindow == null)
 			{
-				className:"alphacube",
-				showProgress: true,
-			});
+				win = new Window(
+				{
+					className:"alphacube",
+					showProgress: true,
+				});
+				var idwindow = win.getId();
+			}
 			var date = new Date(json.date*1000);
 			var date2 = new Date(json.lastupdate*1000);
 			var html = '<div id="vm">';
-			html+='<ul id="tabs_example_one" class="menu1" >';
+			html+='<ul id="tabs_example_one" class="menuvm" >';
 			html+='<li><a href="#overview"><b><img src="img/information.png" alt=""/>Overview</b></a></li>';
 			html+='<li><a href="#cpu"><b><img src="img/cpu.png" alt=""/>CPU</a></b></li>';
 			html+='<li><a href="#ram"><b><img src="img/ram.png" alt=""/>RAM</a></b></li>';
 			html+='<li><a href="#network"><b><img src="img/world.png" alt=""/>Network</a></b></li>';
 			html+='<li><a href="#storage"><b><img src="img/database_gear.png" alt=""/>Storage</a></b></li>';
 			html+='<li><a href="#misc"><b><img src="img/wrench.png" alt=""/>Misc</a></b></li>';
-			html+= '</ul><div id="overview">';
-			html+='<p>State : '+json.state+'</p>';
-			html+='<p>Date of creation : '+date+'</p>';
-			html+='<p>Cap: '+json.cap+'</p>';
-			html+='<p>Weight: '+json.weight+'</p></div>';
+			html+= '</ul><div id="overview" class="text">';
+			
+			html+='<br/><p><b>State: </b>'+json.state+'</p>';
+			html+='<p><b>System: </b>'+json.kernel+'</p>';
+			html+='<p><b>CPU installed: </b>'+json.vcpu_number+'</p>';
+			html+='<p><b>RAM installed: </b>'+json.d_min_ram/(1024*1024)+' MB</p>';
+			html+='<p><b>Date of creation: </b>'+date+'</p>';
+
+			html+='<p><b>Actions: </b><br/><img src="img/pause.png" alt=""/><img src="img/stop.png" alt=""/><img src="img/destroy.png" alt=""/></p>';
+			html+='<p><b>Live migration to: </b>xenb1 &nbsp xena2</p></div>';
+
+			
 			html+='<div id="cpu">';
-			html+='<p>VCPU use : '+json.vcpu_use+'</p>';
-			html+='<p>VCPU at startup : '+json.vcpus_at_startup+'</p>';
-			html+='<p>VCPU number : '+json.vcpu_number+'</p></div>';
+			html+='<br/><p><b>VCPU use:</b> '+json.vcpu_use+'</p>';
+			html+='<p><b>VCPU at startup:</b> '+json.vcpus_at_startup+'</p>';
+			html+='<p><b>VCPU number:</b> '+json.vcpu_number+'</p>';
+			html+='<p><b>Cap:</b> '+json.cap+'</p>';
+			html+='<p><b>Weight:</b> '+json.weight+'</p></div>';
+
+			
 			html+='<div id="ram">';
-			html+='<p>RAM : '+json.d_min_ram/(1024*1024)+' MB</p></div>';
+			html+='<br/><b><p>RAM:</b> '+json.d_min_ram/(1024*1024)+' MB</p></div>';
+			
 			html+='<div id="network"></div>';
+			
 			html+='<div id="storage"></div>';
+			
 			html+='<div id="misc">';
-			html+='<p>On shutdown : '+json.actions_after_shutdown+'</p>';
-			html+='<p>On reboot : '+json.actions_after_reboot+'</p>';
-			html+='<p>On crash : '+json.actions_after_crash+'</p></div>';
+			html+='<br/><b><p>On shutdown:</b> '+json.actions_after_shutdown+'</p>';
+			html+='<b><p>On reboot:</b> '+json.actions_after_reboot+'</p>';
+			html+='<b><p>On crash:</b> '+json.actions_after_crash+'</p></div>';
+			
 			html+='</div>';
-			win.setTitle(name);
+			win.setTitle('<b>'+name+'</b> ('+dom0+')');
 			win.setHTMLContent(html);
 			new Control.Tabs('tabs_example_one');
 			win.show();
-			//win.updateWidth();
-			win.setSize(500,200);
-			//win.updateHeight();
+			win.setSize(500,203);
 		},
 		onFailure: function()
 		{
@@ -104,7 +122,6 @@ function refresh()
 		onComplete: function(transport)
 		{
 			update_portal(transport.responseText.evalJSON());
-			
 			setTimeout(refresh, refresh_time);
 		},
 		onFailure: function()
@@ -155,7 +172,7 @@ function content_dom0(dom0)
 		  
 		result += '<tr id="' + domU.name
 			+ '" onclick="display_vm(\'' + domU.name + '\',\'' + dom0.id
-			+ '\')"><td>' + domU.name + '</td><td>' + domU.state
+			+ '\',\'' + dom0.name +'\')"><td>' + domU.name + '</td><td>' + domU.state
 			+ '</td><td>' + html_cpu_meters(domU.cpu_use)
 			+ '</td></tr>';
 	});
