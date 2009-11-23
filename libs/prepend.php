7290996b78e7a40d499a9d941c87574cae99b3cc
Olivier Lambert <lambert.olivier@gmail.com>
2009-11-23 14:24:53 +0100

    Put GPL header in pertinent files

--- libs/prepend.php
+++ libs/prepend.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * This file is a part of Xen Orchesrta.
+ *
+ * Xen Orchestra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xen Orchestra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xen Orchestra. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Xen Orchestra
+ * @license http://www.gnu.org/licenses/gpl-3.0-standalone.html GPLv3
+ **/
 // The session is used for users management.
 session_name('XO');
 session_set_cookie_params(31*24*3600); // One month.
